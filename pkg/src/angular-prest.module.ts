3a0b6e83ce9a4262705168034d978e64e3acd9d7
Tiago Temporin <tiaguinhocab@gmail.com>
2017-12-09 10:14:26 +0000

    add comments and new packges for angular 4.4.6+
    Author:    Tiago Temporin <tiaguinhocab@gmail.com>

--- pkg/src/angular-prest.module.ts
+++ pkg/src/angular-prest.module.ts
@@ -1,25 +1,17 @@
-import {
-	NgModule,
-	ModuleWithProviders
-} from '@angular/core';
-
-import { Http } from '@angular/http';
+import { NgModule, ModuleWithProviders } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-import  { AngularPrestService } from './angular-prest.service';
-
-export interface PrestConfig  {
-	localStorageData: string;
-	baseUrl: string;
-	tokenPath: string;
-}
+import { HttpClientModule } from '@angular/common/http';
+import { AngularPrestService } from './angular-prest.service';
+import { PrestConfig } from './angular-prest.config';
 
 @NgModule({
-	imports: [
-		CommonModule
-	]
+	imports: [CommonModule, HttpClientModule]
 })
 export class AngularPrestModule {
+	/**
+	 * set configurations to the module
+	 * @param config PrestConfig
+	 */
 	static forRoot(config: PrestConfig): ModuleWithProviders {
 		return {
 			ngModule: AngularPrestModule,
